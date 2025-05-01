cdbb7b0dcdd7fca073104ba88f565758a8707bfd
Matilda Söderberg <matilda.89@live.se>
2025-05-01 16:07:47 +0200

    Added jsx file with updated html

---
+++ src/Card.jsx
@@ -0,0 +1,20 @@
+import react from 'react';
+import './card.css';
+
+export default function Card (){
+    return(
+        <>
+        <div>
+            <form>
+            <h4>Term</h4>
+            <p>Definition</p>
+            <label>Choose a photo
+                <small>(optional)</small>
+            </label>
+            <input type='file' accept="image/png, image/jpeg"/>
+            <button>+</button>
+            </form>
+        </div>
+        </>
+    )
+}
