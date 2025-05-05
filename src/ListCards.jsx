e6487db907695fa47efcd0226dee00e08f05d689
Tillsxp <matilda.89@live.se>
2025-05-05 14:57:35 +0200

    Fixed onClick flip animation for flash-card

--- src/ListCards.jsx
+++ src/ListCards.jsx
@@ -1,21 +1,20 @@
-import react from 'react';
 import React, { useEffect, useState } from "react";
 import cong from "./Configuration";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { ref, onValue } from "firebase/database";
+import './listCards.css';
 
 export default function ListCards() {
     const [data, setData] = useState([]);
-    
+    const [flippedCards, setFlippedCards] = useState({});
+
     useEffect(() => {
         
-        const database = getDatabase(cong);
+        const collectionRef = ref(cong, "flash_cards");
 
-        const collectionRef = ref(database, "flash_cards");
-
-            const fetchData = () => {
+        const fetchData = () => {
             onValue(collectionRef, (snapshot) => {
             const dataItem = snapshot.val();
-            console.log("Fetched Data:", dataItem);
+            
 
             if (dataItem) {
                 setData([dataItem]);
@@ -26,16 +25,34 @@ export default function ListCards() {
   fetchData();
 }, []);
 
+const toggleFlip = (term) => {
+  setFlippedCards((prevState) => ({
+    ...prevState,
+    [term]: !prevState[term],
+  }));
+};
+
 return (
   <div>
     <ul>
       {data.map((item, index) => (
         <li key={index}>
-          <h4>{item.Term}</h4>
-          <p>{item.Definition}</p>
-           {item.Photo && item.Photo.trim() !== "" && (
-            <img src={item.Photo} alt={item.Term || "Image"}/>
-           )}
+            <div className={`card ${flippedCards[item.Term] ? "flipped" : ""}`}
+              onClick={() => toggleFlip(item.Term)} >
+              <div className="outter-box">
+                <div className="front">
+                  <h4>{item.Term}</h4>
+                </div>
+                <div className="back">
+                <div className="inner-box">
+                    <p>{item.Definition}</p>
+                  {item.Photo && item.Photo.trim() !== "" && (
+                    <img src={`/assets/${item.Photo}`} alt={item.Term || "Image"}/>
+                    )}
+                </div>
+                </div>
+              </div>
+            </div>
         </li>
       ))}
     </ul>
