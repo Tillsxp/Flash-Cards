5389f2fc0475841d581344ce2ae041f115e68d41
Tillsxp <matilda.89@live.se>
2025-05-08 11:32:36 +0200

    Updated dataItem to liste properly after adjustment in firebase json

--- src/ListCards.jsx
+++ src/ListCards.jsx
@@ -17,7 +17,8 @@ export default function ListCards() {
             
 
             if (dataItem) {
-                setData([dataItem]);
+              
+                setData(Object.values(dataItem));
       }
     });
   };
